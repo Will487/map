955122a6f1cddfe7018aa7cf67d790b5ee34d19d
William Wallace de Oliveira <willoliveira268@gmail.com>
2024-09-13 14:54:41 -0300

    commit mapas com react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import MapPage from "./pages/MapPage";
+
+export const REACT_APP_GOOGLE_API_KEY = "AIzaSyCJ7HpA4oSML6ieMYUHWnzv3RQtZCQF6cg";
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
